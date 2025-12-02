85f66f22260a71160ec0afc8596a9cc8d15733e4
shahriyor <islomovrofe@gmail.com>
2025-12-02 19:28:49 +0500

    all_cart

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
 from django.views import View
 from .models import Order, OrderItem, Cart, CartItem
 
